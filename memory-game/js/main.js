e717739d6e435e1df518fe72ed7a1bf4e08259e0
mervyns <mervyns@gmail.com>
2018-08-08 12:05:30 +0800

    created game logic unit 9

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,19 +1,21 @@
-var cards = ["queen", "queen", "queen", "king"];
+var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
 
-console.log("User Flipped " + cardOne);
-
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-
-console.log("User Flipped " + cardTwo);
-
-if (cardsInPlay.length === 2) {
-  if (cardOne === cardTwo) {
-    alert("You found a match!");
-  } else {
-    alert("Sorry try again!");
+var checkForMatch = function() {
+  if (cardsInPlay.length === 2) {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+      alert("You found a match!");
+    } else {
+      alert("You did not find a match! Try again!");
+    }
   }
-}
+};
+
+var flipCard = function(cardId) {
+  console.log("User flipped " + cards[cardId]);
+  cardsInPlay.push(cards[cardId]);
+  checkForMatch();
+};
+
+flipCard(0);
+flipCard(2);
